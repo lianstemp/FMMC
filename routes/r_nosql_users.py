4f1a4faf83457c986009c500d07284943de0f1b1
MRXAZK <farhanauliandastudy@gmail.com>
2022-11-12 00:05:50 +0700

    Add NOSQL Endpoint

---
+++ routes/r_nosql_users.py
@@ -0,0 +1,67 @@
+
+import os
+from fastapi import FastAPI, Body, HTTPException, status, APIRouter
+from fastapi.responses import Response, JSONResponse
+from fastapi.encoders import jsonable_encoder
+from schemas.s_nosql_users import UserModel, UpdateUserModel
+from typing import Optional, List
+import motor.motor_asyncio
+
+
+user_nosql = APIRouter()
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.users
+
+@user_nosql.post("/nosql/user/", response_description="Add new user", response_model=UserModel, tags=["NoSQL Database"])
+async def create_user(user: UserModel = Body(...)):
+    user = jsonable_encoder(user)
+    new_user = await db["users"].insert_one(user)
+    created_user = await db["users"].find_one({"_id": new_user.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
+
+
+@user_nosql.get(
+    "/nosql/user/", response_description="List all users", response_model=List[UserModel], tags=["NoSQL Database"]
+)
+async def list_users():
+    users = await db["users"].find().to_list(1000)
+    return users
+
+
+@user_nosql.get(
+    "/nosql/user/{id}", response_description="Get a single user", response_model=UserModel, tags=["NoSQL Database"]
+)
+async def show_user_with_id(id: str):
+    if (user := await db["users"].find_one({"_id": id})) is not None:
+        return user
+
+    raise HTTPException(status_code=404, detail=f"User {id} not found")
+
+
+@user_nosql.put("/nosql/user/{id}", response_description="Update a user", response_model=UserModel, tags=["NoSQL Database"])
+async def update_user(id: str, user: UpdateUserModel = Body(...)):
+    user = {k: v for k, v in user.dict().items() if v is not None}
+
+    if len(user) >= 1:
+        update_result = await db["users"].update_one({"_id": id}, {"$set": user})
+
+        if update_result.modified_count == 1:
+            if (
+                updated_user := await db["users"].find_one({"_id": id})
+            ) is not None:
+                return updated_user
+
+    if (existing_user := await db["users"].find_one({"_id": id})) is not None:
+        return existing_user
+
+    raise HTTPException(status_code=404, detail=f"User {id} not found")
+
+
+@user_nosql.delete("/nosql/user/{id}", response_description="Delete a user", tags=["NoSQL Database"])
+async def delete_user(id: str):
+    delete_result = await db["users"].delete_one({"_id": id})
+
+    if delete_result.deleted_count == 1:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"User {id} not found")
---
+++ schemas/s_nosql_users.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel, Field, EmailStr
+from bson import ObjectId
+from typing import Optional, List
+from datetime import date
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class UserModel(BaseModel):
+    id_users: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(..., min_lenght=3, max_length=255)
+    address: str = Field(..., min_lenght=3, max_length=255)
+    date_of_birth: date = Field(...)
+    telp: str = Field(..., min_lenght=3, max_length=255)
+    email: EmailStr = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Farhan Aulianda",
+                "address": "Banda Aceh",
+                "date_of_birth": "1996-07-01",
+                "telp": "(+62) 822 8040 7563",
+                "email": "farhanauliandastudy@gmail.com"
+            }
+        }
+
+
+class UpdateUserModel(BaseModel):
+    name: Optional[str]
+    address: Optional[str]
+    telp: Optional[str]
+    email: Optional[EmailStr]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Farhan Aulianda",
+                "address": "Banda Aceh",
+                "date_of_birth": "1996-07-01",
+                "telp": "(+62) 822 8040 7563",
+                "email": "farhanauliandastudy@gmail.com"
+            }
+        }
